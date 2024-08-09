47d1f31eaf4ef0ff491e862066b1d4e336793417
MoMakkawi <MoMakkawi@hotmail.com>
2024-08-09 18:05:02 +0300

    Add my sql config and context.

---
+++ Scheduler/Data/MySQLDBContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Scheduler.Data;
+
+public class MySQLDBContext(DbContextOptions options) : DbContext(options)
+{
+}
---
+++ Scheduler/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+using Scheduler.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var connectionString = builder.Configuration.GetConnectionString("MySQLConnectionString");
+builder.Services.AddDbContext<MySQLDBContext>(
+    options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
